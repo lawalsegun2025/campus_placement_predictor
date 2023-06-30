dc0f38181f4f7aedf2d45a151bcaca17024a9a41
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-06-30 12:26:57 +0100

    complete

--- app.py
+++ app.py
@@ -25,3 +25,33 @@ def hello_world():
         mba = float(my_dict['mba'])
         input_features = [[gender, spec, tech, work, ssc, hsc, dsc, mba]]
         
+        # Predictiong the class of either 0 or 1
+        predicted_class = lr_2.predict(input_features)
+
+        # predict the probability
+        predicted_prob = lr_2.predict_proba(input_features)
+
+        print(predicted_class, predicted_prob[0][0])
+
+        if predicted_class[0] == 1:
+            proba = predicted_prob[0][1]
+        else:
+            proba = predicted_prob[0][0]
+
+        print(predicted_class, proba*100)
+
+        place_map = {1: "Will Be Placed", 0: "Beter Luck Next Time"}
+
+        predicted_classs_end = place_map[predicted_class[0]]
+
+        if predicted_class[0] == 1:
+            return render_template('show.html', predicted_classs_end=predicted_classs_end, predicted_prob=round(proba*100, 2), placed=True)
+
+        else:
+            return render_template('show.html', predicted_classs_end=predicted_classs_end)
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
