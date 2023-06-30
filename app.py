fc7e6416660e2642025d96bf764cf3048c5e25ae
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-06-30 12:07:04 +0100

    renamed

--- app.py
+++ app.py
@@ -6,3 +6,22 @@ import pickle
 file = open("cp_predictor.pkl", "rb")
 lr_2 = pickle.load(file)
 file.close()
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def hello_world():
+
+    if request.method == "POST":
+
+        my_dict = request.form
+        gender = int(my_dict['gender'])
+        spec = int(my_dict['spec'])
+        tech = int(my_dict['tech'])
+        work = int(my_dict['work'])
+        ssc = float(my_dict['ssc'])
+        hsc = float(my_dict['hsc'])
+        dsc = float(my_dict['dsc'])
+        mba = float(my_dict['mba'])
+        input_features = [[gender, spec, tech, work, ssc, hsc, dsc, mba]]
+        
